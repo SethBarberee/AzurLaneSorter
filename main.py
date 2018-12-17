2ea9f0ae54dc8ba5a4cc31abe51a884fe1dfb248
Seth Barberee <seth.barberee@gmail.com>
2018-12-16 23:11:08 -0600

    initial sorting of backline and frontline working

--- main.py
+++ main.py
@@ -1,14 +1,22 @@
 import json
 def parse_data():
+    backline = []
+    frontline = []
     # Open the data file for reading
     # loop through line by line
     with open('data.json', 'r') as f:
         ships_dict = json.load(f)
-    # TODO parse data from text file into respective arrays
-    # TODO concatenate into a list of 6 ships
     # For now, print each ship's name from our json file
     for ship in ships_dict:
-        print(ship['Name'])
+        # Parse the class for backline/frontline
+        if(ship['Class'] != 'BB' and ship['Class'] != 'BC' and ship['Class'] != 'CV' and ship['Class'] != 'CVL'):
+            frontline.append(ship['Name'])
+        else:
+            backline.append(ship['Name'])
+    print("Frontline:")
+    print(frontline)
+    print("Backline:")
+    print(backline)
 
 def main():
     parse_data()
