51882d2c8020115971664bedd53a867d3c9ed3c9
Seth Barberee <seth.barberee@gmail.com>
2020-03-29 10:23:23 -0500

    actually filter ships by rarity or nation

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import json, sys, os, copy
 ship_dict = [] # global dictionary for all the ships
 menu_actions = {}
-
+valid_nations = ['Eagle Union', 'Royal Navy', 'Ironblood', 'Sakura Empire', 'Dragon Empery', 'Sardegna Empire', 'Northern Parliament', 'Iris Libre', 'Vichya Dominion']
+valid_rarity =  ['Common', 'Rare', 'Elite', 'Super Rare', 'Ultra']
 # Execute menu
 def exec_menu(choice):
     os.system('clear')
@@ -31,8 +32,10 @@ def parse_data(filename='data.json'):
 def new_ship_data(ships_dict):
     """ Add a new ship dictionary and return the updated list"""
     # Prompt each stat
+    _id = input("ID: ")
     name = input("Name: ")
     # TODO check nation
+    rarity = input("Rarity: ")
     nation = input("Nation: ")
     clas = input("Class: ")
     luck = int(input("Luck: "))
@@ -53,7 +56,9 @@ def new_ship_data(ships_dict):
     print("Adding " + name + " to the list")
     # Create the dictionary for the new ship
     new_ship = {
+            "ID": _id,
             "Name": name,
+            "Rarity": rarity,
             "Nation": nation,
             "Class": clas,
             "Luck": luck,
@@ -85,17 +90,46 @@ def find_line(ships_dict):
     """ Given the dictionary, parse frontline and backline """
     backline = []
     frontline = []
+    subline = []
     # These are the classes that go in the backline
     backline_set = {'BB', 'BC', 'CV', 'CVL', 'AR', 'CM'}
+    subline_set = {'SS'}
     for ship in ships_dict:
         if ship['Class'] in backline_set:
             # Matches one of the classes so add it
             backline.append(ship)
+        elif ship['Class'] in subline_set:
+            # Found a submarine
+            subline.append(ship)
         else:
             # Doesn't match so it's in the front
             frontline.append(ship)
     # Return which line they are in respectively
-    return (backline, frontline)
+    return (backline, frontline, subline)
+
+def filter_ships(ships_dict, filter_name='Nation'):
+    """ Filter the ship dictionary based on Nation or Rarity """
+    new_ship_dict = []
+    if filter_name == "Nation":
+        # TODO check if it is a valid nation
+        nation = input('Enter the desired nation: ')
+        print("Filtering by " + nation)
+        for ship in ships_dict:
+            if(ship["Nation"] == nation):
+                new_ship_dict.append(ship)
+        return new_ship_dict
+            
+    elif filter_name == "Rarity":
+        # TODO check if it's valid rarity
+        rarity = input('Enter the desired rarity: ')
+        print("Filtering by " + rarity)
+        for ship in ships_dict:
+            if(ship["Rarity"] == rarity):
+                new_ship_dict.append(ship)
+        return new_ship_dict
+    else:
+        return
+
 
 # TODO: only sorts by one stat... make it do more
 def sort_ships(ships_dict, stat='HP'):
@@ -130,17 +164,27 @@ def menu1():
 
 def menu2():
     """ Menu that sorts our data however we like to our top 3 for front and back"""
-    # TODO find a way to filter based on Nation if desired
     # TODO What if we want certain ships in our lineup??
     global ship_dict
     stat = input('Enter the stat you would like to sort by: ')
     print("Sorting by " + stat)
 
     # Split dictionary into backline and frontline
-    (backline, frontline) = find_line(ship_dict)
+    (backline, frontline, subline) = find_line(ship_dict)
+    # TODO make it optional to filter instead of the force-filtering
     # Sort both by respective stat
     new_backline = sort_ships(backline, stat)
+    print("filter the backline")
+    new_backline = filter_ships(new_backline)
+
     new_frontline = sort_ships(frontline, stat)
+    print("filter the frontline")
+    new_frontline = filter_ships(new_frontline)
+
+    new_subline = sort_ships(subline, stat)
+    print("Filter the subs")
+    new_subline = filter_ships(new_subline)
+ 
     if new_backline == None or new_frontline == None:
         print("Load ships please")
         # TODO maybe a delay
@@ -149,10 +193,13 @@ def menu2():
     # Create a list of names for each line
     frontline_names = []
     backline_names = []
+    subline_names = []
     oil_cost = 0
+    suboil_cost = 0 # subs are separate oil
     # See how many ships we have in each lineup
     max_range_back = len(new_backline)
     max_range_front = len(new_frontline)
+    max_range_sub = len(new_subline)
     # Backline
     if(max_range_back >= 3):
         # Only do the top three
@@ -161,6 +208,7 @@ def menu2():
             backline_names.append(new_backline[i]['Name'])
             oil_cost += int(float(new_backline[i]['Cost']))
     else:
+        print(max_range_back)
         for i in range(max_range_back):
             # Add the backline ship and cost
             backline_names.append(new_backline[i]['Name'])
@@ -173,10 +221,23 @@ def menu2():
             frontline_names.append(new_frontline[i]['Name'])
             oil_cost += int(float(new_frontline[i]['Cost']))
     else:
-        for i in range(max_range_back):
+        for i in range(max_range_front):
             # Add the frontline ship and cost
             frontline_names.append(new_frontline[i]['Name'])
             oil_cost += int(float(new_frontline[i]['Cost']))
+
+    if(max_range_sub >= 3):
+        # Only do the top three
+        for i in range(3):
+            # Add the frontline ship and cost
+            subline_names.append(new_subline[i]['Name'])
+            suboil_cost += int(float(new_subline[i]['Cost']))
+    else:
+        for i in range(max_range_sub):
+            # Add the frontline ship and cost
+            frontline_names.append(new_subline[i]['Name'])
+            suboil_cost += int(float(new_subline[i]['Cost']))
+
 
     # Print each line
     print("Backline:")
@@ -185,6 +246,10 @@ def menu2():
     print(frontline_names)
     print("Cost: ")
     print(oil_cost)
+    print("Subs:")
+    print(subline_names)
+    print("Sub Cost: ")
+    print(suboil_cost)
     choice = input("Enter 0 to exit or main for main menu: ")
     exec_menu(choice)
     return 
