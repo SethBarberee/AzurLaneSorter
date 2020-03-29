fd8b266dc4d27896043a8eefe5eba2ce30d14a8f
Seth Barberee <seth.barberee@gmail.com>
2020-03-29 12:09:25 -0500

    refactor to a separate utils file for some functions

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-import json, sys, os, copy
+import json, sys, os, copy # global
+
+import utils #local
+
 ship_dict = [] # global dictionary for all the ships
 menu_actions = {}
 valid_nations = ['Eagle Union', 'Royal Navy', 'Ironblood', 'Sakura Empire', 'Dragon Empery', 'Sardegna Empire', 'Northern Parliament', 'Iris Libre', 'Vichya Dominion']
@@ -86,27 +89,6 @@ def new_ship_data(ships_dict):
         json.dump(ships_dict, write_file, indent = 4, separators=(',',': '))
     return ships_dict
 
-def find_line(ships_dict):
-    """ Given the dictionary, parse frontline and backline """
-    backline = []
-    frontline = []
-    subline = []
-    # These are the classes that go in the backline
-    backline_set = {'BB', 'BC', 'CV', 'CVL', 'AR', 'CM'}
-    subline_set = {'SS'}
-    for ship in ships_dict:
-        if ship['Class'] in backline_set:
-            # Matches one of the classes so add it
-            backline.append(ship)
-        elif ship['Class'] in subline_set:
-            # Found a submarine
-            subline.append(ship)
-        else:
-            # Doesn't match so it's in the front
-            frontline.append(ship)
-    # Return which line they are in respectively
-    return (backline, frontline, subline)
-
 def filter_ships(ships_dict, filter_name='Nation'):
     """ Filter the ship dictionary based on Nation or Rarity """
     new_ship_dict = []
@@ -130,28 +112,6 @@ def filter_ships(ships_dict, filter_name='Nation'):
     else:
         return
 
-
-# TODO: only sorts by one stat... make it do more
-def sort_ships(ships_dict, stat='HP'):
-    """ Sort the ship dictionary given a certain stat """
-    # First, check if the stat is in the dictionary
-    try:
-        if stat in ships_dict[0]:
-            if stat == 'Armor':
-                # Armor only has three values
-                keyorder = ['Heavy', 'Medium', 'Light' ]
-                order = {key: i for i, key in enumerate(keyorder)}
-                sorted_ship_dict = sorted(ships_dict, key = lambda d: order[d[stat]])
-            else:
-                sorted_ship_dict = sorted(ships_dict, key = lambda d: d[stat], reverse=True)
-            return sorted_ship_dict
-        else:
-            print("Key doesn't exist")
-            return
-    except:
-        return
-
-
 def menu1():
     """ Menu that will import the ship data """
     # Declare we are using the global to add the JSON data to it
@@ -170,18 +130,18 @@ def menu2():
     print("Sorting by " + stat)
 
     # Split dictionary into backline and frontline
-    (backline, frontline, subline) = find_line(ship_dict)
+    (backline, frontline, subline) = utils.find_line(ship_dict)
     # TODO make it optional to filter instead of the force-filtering
     # Sort both by respective stat
-    new_backline = sort_ships(backline, stat)
+    new_backline = utils.sort_ships(backline, stat)
     print("filter the backline")
     new_backline = filter_ships(new_backline)
 
-    new_frontline = sort_ships(frontline, stat)
+    new_frontline = utils.sort_ships(frontline, stat)
     print("filter the frontline")
     new_frontline = filter_ships(new_frontline)
 
-    new_subline = sort_ships(subline, stat)
+    new_subline = utils.sort_ships(subline, stat)
     print("Filter the subs")
     new_subline = filter_ships(new_subline)
  
@@ -189,67 +149,9 @@ def menu2():
         print("Load ships please")
         # TODO maybe a delay
         menu_actions['main']()
+    
+    utils.create_line(new_backline, new_frontline, new_subline)
 
-    # Create a list of names for each line
-    frontline_names = []
-    backline_names = []
-    subline_names = []
-    oil_cost = 0
-    suboil_cost = 0 # subs are separate oil
-    # See how many ships we have in each lineup
-    max_range_back = len(new_backline)
-    max_range_front = len(new_frontline)
-    max_range_sub = len(new_subline)
-    # Backline
-    if(max_range_back >= 3):
-        # Only do the top three
-        for i in range(3):
-            # Add the backline ship and cost
-            backline_names.append(new_backline[i]['Name'])
-            oil_cost += int(float(new_backline[i]['Cost']))
-    else:
-        print(max_range_back)
-        for i in range(max_range_back):
-            # Add the backline ship and cost
-            backline_names.append(new_backline[i]['Name'])
-            oil_cost += int(float(new_backline[i]['Cost']))
-    # Frontline 
-    if(max_range_front >= 3):
-        # Only do the top three
-        for i in range(3):
-            # Add the frontline ship and cost
-            frontline_names.append(new_frontline[i]['Name'])
-            oil_cost += int(float(new_frontline[i]['Cost']))
-    else:
-        for i in range(max_range_front):
-            # Add the frontline ship and cost
-            frontline_names.append(new_frontline[i]['Name'])
-            oil_cost += int(float(new_frontline[i]['Cost']))
-
-    if(max_range_sub >= 3):
-        # Only do the top three
-        for i in range(3):
-            # Add the frontline ship and cost
-            subline_names.append(new_subline[i]['Name'])
-            suboil_cost += int(float(new_subline[i]['Cost']))
-    else:
-        for i in range(max_range_sub):
-            # Add the frontline ship and cost
-            frontline_names.append(new_subline[i]['Name'])
-            suboil_cost += int(float(new_subline[i]['Cost']))
-
-
-    # Print each line
-    print("Backline:")
-    print(backline_names)
-    print("Frontline:")
-    print(frontline_names)
-    print("Cost: ")
-    print(oil_cost)
-    print("Subs:")
-    print(subline_names)
-    print("Sub Cost: ")
-    print(suboil_cost)
     choice = input("Enter 0 to exit or main for main menu: ")
     exec_menu(choice)
     return 
---
+++ utils.py
@@ -0,0 +1,115 @@
+# Mainly a utility file with the following
+
+# find_Line
+# Figure out which line a ship belongs to
+
+# sort_ships
+# Sort the dictionary based on a stat
+
+# create_line
+# Filter best three for each line and calculate oil cost
+
+def find_line(ships_dict):
+    """ Given the dictionary, parse frontline and backline """
+    backline = []
+    frontline = []
+    subline = []
+    # These are the classes that go in the backline
+    backline_set = {'BB', 'BC', 'CV', 'CVL', 'AR', 'CM'}
+    subline_set = {'SS'}
+    for ship in ships_dict:
+        if ship['Class'] in backline_set:
+            # Matches one of the classes so add it
+            backline.append(ship)
+        elif ship['Class'] in subline_set:
+            # Found a submarine
+            subline.append(ship)
+        else:
+            # Doesn't match so it's in the front
+            frontline.append(ship)
+    # Return which line they are in respectively
+    return (backline, frontline, subline)
+
+# TODO: only sorts by one stat... make it do more
+def sort_ships(ships_dict, stat='HP'):
+    """ Sort the ship dictionary given a certain stat """
+    # First, check if the stat is in the dictionary
+    try:
+        if stat in ships_dict[0]:
+            if stat == 'Armor':
+                # Armor only has three values
+                keyorder = ['Heavy', 'Medium', 'Light' ]
+                order = {key: i for i, key in enumerate(keyorder)}
+                sorted_ship_dict = sorted(ships_dict, key = lambda d: order[d[stat]])
+            else:
+                sorted_ship_dict = sorted(ships_dict, key = lambda d: d[stat], reverse=True)
+            return sorted_ship_dict
+        else:
+            print("Key doesn't exist")
+            return
+    except:
+        return
+
+def create_line(backline, frontline, subline):
+    # Create a list of names for each line
+    frontline_names = []
+    backline_names = []
+    subline_names = []
+    oil_cost = 0
+    suboil_cost = 0 # subs are separate oil
+    # See how many ships we have in each lineup
+    max_range_back = len(backline)
+    max_range_front = len(frontline)
+    max_range_sub = len(subline)
+    # Backline
+    if(max_range_back >= 3):
+        # Only do the top three
+        for i in range(3):
+            # Add the backline ship and cost
+            backline_names.append(backline[i]['Name'])
+            oil_cost += int(float(backline[i]['Cost']))
+    else:
+        print(max_range_back)
+        for i in range(max_range_back):
+            # Add the backline ship and cost
+            backline_names.append(backline[i]['Name'])
+            oil_cost += int(float(backline[i]['Cost']))
+    # Frontline 
+    if(max_range_front >= 3):
+        # Only do the top three
+        for i in range(3):
+            # Add the frontline ship and cost
+            frontline_names.append(frontline[i]['Name'])
+            oil_cost += int(float(frontline[i]['Cost']))
+    else:
+        for i in range(max_range_front):
+            # Add the frontline ship and cost
+            frontline_names.append(frontline[i]['Name'])
+            oil_cost += int(float(frontline[i]['Cost']))
+
+    if(max_range_sub >= 3):
+        # Only do the top three
+        for i in range(3):
+            # Add the frontline ship and cost
+            subline_names.append(subline[i]['Name'])
+            suboil_cost += int(float(subline[i]['Cost']))
+    else:
+        for i in range(max_range_sub):
+            # Add the frontline ship and cost
+            frontline_names.append(subline[i]['Name'])
+            suboil_cost += int(float(subline[i]['Cost']))
+
+
+    # Print each line
+    print("Backline:")
+    print(backline_names)
+    print("Frontline:")
+    print(frontline_names)
+    print("Cost: ")
+    print(oil_cost)
+    print("Subs:")
+    print(subline_names)
+    print("Sub Cost: ")
+    print(suboil_cost)
+    return    
+
