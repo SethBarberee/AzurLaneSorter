a2bd101700989ca73afcca69de757e787d95d8d9
Seth Barberee <seth.barberee@gmail.com>
2020-04-22 13:00:16 -0500

    working download/set of images per ship

--- line_widget.py
+++ line_widget.py
@@ -4,6 +4,7 @@ from PyQt5.QtGui import QIcon, QPixmap
 import os.path
 from os import path
 
+import requests
 import urllib
 
 class UIShipWidget(QWidget):
@@ -20,15 +21,34 @@ class UIShipWidget(QWidget):
         # TODO check if pic storage exists
         image_dir = "picStorage"
         name_image = label.replace(" (Retrofit)", "Kai")
-        image_download = "https://azurlane.koumakan.jp/File:" + name_image.replace(" ", "_") + "Icon.png"
         image_path = image_dir + "/" + name_image + "Icon.png"
 
-        print("testing path:")
-        print(image_path)
-        # TODO uncomment when we get downloading the image working versus downloading the page
-        #urllib.request.urlretrieve(image_download, image_path)
-        # TODO we'll set to just Hood for now
-        image_path = image_dir + "/" + "Hood" + "Icon.png"
+        # Create requests session to get the image
+        # Taken from MediaWiki API page:
+        # https://www.mediawiki.org/wiki/API:Allimages
+
+        S = requests.Session()
+
+        URL = "https://azurlane.koumakan.jp/w/api.php" # Base URL for Azur Lane Wiki
+
+        PARAMS = {
+            "action": "query",
+            "format": "json",
+            "list": "allimages",
+            "aifrom": name_image + "Icon", # Azur Wiki follows a simple format for icons
+            "ailimit": "1" # We only need the top result
+        }
+
+        R = S.get(url=URL, params=PARAMS)
+        DATA = R.json()
+
+        IMAGES = DATA["query"]["allimages"]
+
+        image_download = IMAGES[0]["url"] # we have the right URL so let's download
+
+        # Download and set
+        urllib.request.urlretrieve(image_download, image_path)
+        # All good so set the image
         self.Picture.setPixmap(QPixmap(image_path))
 
 
